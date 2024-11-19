7176c13b7689806392396f6baf138246a07d2cd5
umezaki <umezaki@fullmarks.co.jp>
2024-11-19 11:41:08 +0900

    COMMIT_EDITMSGのコミットメッセージをチェック

--- code_checker/batch_yaml_checker.py
+++ code_checker/batch_yaml_checker.py
@@ -1,7 +1,5 @@
-import pathlib
 import sys
 
-import git
 from ruamel.yaml import YAML
 
 # typ="rt" → コメントを保持する
@@ -12,10 +10,17 @@ yaml.preserve_quotes = True
 
 class BatchYamlChecker:
     @classmethod
-    def check_batch_yaml(cls, yaml_path: str):
-        repo = git.Repo(cls._get_root_path(yaml_path=yaml_path))
-        print(repo.head.commit.message)
-        exit(1)
+    def check_batch_yaml(cls, yaml_path: str, commit_message_file_path: str):
+        # clubjt-server/.git/COMMIT_EDITMSGに記載されたコミットメッセージに「batch.yaml変更」が含まれない場合は、batch.yamlのコミットを中断する。
+ 
+        with open(commit_message_file_path, "r") as f:
+            commit_message = f.read()
+
+        if yaml_path.endswith("batch.yaml") and "batch.yaml変更" not in commit_message:
+            print(
+                "[ERROR] batch.yamlの変更は制限されています。batch2.yamlを変更するか、コミットメッセージに「batch.yaml変更」を記載してください。"
+            )
+            exit(1)
 
         with open(yaml_path, "r") as f:
             definition = yaml.load(f)
@@ -43,32 +48,10 @@ class BatchYamlChecker:
 
         sys.exit(int(is_error))
 
-    @classmethod
-    def _get_root_path(cls, yaml_path: str):
-        """
-        clubjt-serverのルートディレクトリを取得
-        """
-
-        # clubjt-cdk/def/batch/batch.yamlが渡される想定なので、clubjt-cdkでチェックが終了する。
-        # 一つ上のclubjt-serverもチェックするようにする
-        current = pathlib.Path(f"clubjt-server/{yaml_path}")
-
-        print("---------------------")
-
-        while current.parent != current:
-            if current.name == "clubjt-server":
-                return current.parent.resolve()
-
-            current = current.parent
-
-        raise Exception("clubkt-serverディレクトリが見つかりませんでした")
-
 
 def main():
-    if len(sys.argv) < 2:
-        print("[ERROR] Batch定義のYamlファイルが指定されていません")
+    if len(sys.argv) < 3:
+        print("[ERROR] 引数が不足しています")
         sys.exit(1)
 
-    with open(sys.argv[1], "r") as f:
-        print(f.read())
-    BatchYamlChecker.check_batch_yaml(yaml_path=sys.argv[2])
+    BatchYamlChecker.check_batch_yaml(yaml_path=sys.argv[1], commit_message_file_path=sys.argv[2])
