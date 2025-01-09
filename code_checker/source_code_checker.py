1a5540acfe9bb43acc9a36c23544b7d22cab213d
FMs-umezaki <umezaki@fullmarks.co.jp>
2025-01-09 17:51:16 +0900

    エスケープ修正

--- code_checker/source_code_checker.py
+++ code_checker/source_code_checker.py
@@ -44,7 +44,7 @@ class SourceCodeChecker:
         if re.search(r"(if|elif).+\.(created_at|createdAt|updatedAt|updated_at).+", source_code):
             print(f"[WARNING] if・elifでcreate_at・updated_atの値を比較しないでください。")
 
-        if re.search(r"Entity¥.[a-zA-Z0-9_]¥.+is_¥(", source_code):
+        if re.search(r"Entity\.[a-z0-9_]+\.is_\(", source_code):
             print(f"[ERROR] Entity.column.is_(...)を使用しないでください。")
             is_error = True
 
