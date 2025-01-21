6256bd06dfd9e18c921c9d49195b29a596e1dcb5
FMs-umezaki <umezaki@fullmarks.co.jp>
2025-01-21 16:30:25 +0900

    一旦停止

--- code_checker/source_code_checker.py
+++ code_checker/source_code_checker.py
@@ -45,9 +45,9 @@ class SourceCodeChecker:
         # if re.search(r"(if|elif).+\.(created_at|createdAt|updatedAt|updated_at).+", source_code):
         #     print(f"[WARNING] if・elifでcreate_at・updated_atの値を比較しないでください。")
 
-        if re.search(r"Entity¥.[a-zA-Z0-9_]¥.+is_¥(", source_code):
-            print(f"[ERROR] Entity.column.is_(...)を使用しないでください。")
-            is_error = True
+        # if re.search(r"Entity¥.[a-zA-Z0-9_]¥.+is_¥(", source_code):
+        #     print(f"[ERROR] Entity.column.is_(...)を使用しないでください。")
+        #     is_error = True
 
         # ファイルの差分に `# type: ignore` が含まれているかチェック
         diff = subprocess.run(
