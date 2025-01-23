9f23b0e1cbe2542100ba84d94cc30536e04c0ab2
AkamineTaito <akamine@fullmarks.co.jp>
2025-01-23 14:52:08 +0900

    INSIDE_JTOC-6631 【内部】GitHub Actionsを用いて、チェックがついてないコメントが含まれるPRをFailedにする
    - 修正

--- code_checker/pr_status_checker.py
+++ code_checker/pr_status_checker.py
@@ -44,11 +44,12 @@ class PRStatusChecker:
     @classmethod
     def _get_source_branch(cls) -> str | None:
         """マージ元のブランチ名取得"""
+        print(os.environ.get("GIT_REFLOG_ACTION", ""))
+        print("")
         if not os.environ.get("GIT_REFLOG_ACTION", "").startswith("merge "):
             return None
 
-        merged_branch_name = os.environ.get("GIT_REFLOG_ACTION", "").removeprefix("merge ")
-        print("ブランチ名：", merged_branch_name)
+        merged_branch_name = os.environ.get("GIT_REFLOG_ACTION", "").removeprefix("merge origin/")
         return merged_branch_name
 
     @classmethod
